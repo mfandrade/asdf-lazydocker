564ff556d36f4d2b38c168a9693f6f2082d2ff72
Marcelo F Andrade <mfandrade@gmail.com>
2025-07-07 21:48:35 -0400

    docs: todo comment

--- template/lib/utils.bash
+++ template/lib/utils.bash
@@ -72,11 +72,10 @@ get_extension() {
 
 	case "$(uname | tr '[:upper:]' '[:lower:]')" in
 	darwin) extension="tar.gz" ;;
-	freebsd) extension="tar.gz" ;;
 	linux) extension="tar.gz" ;;
 	windows) extension="zip" ;;
 	*)
-		fail "Platform '$(uname)' not supported!"
+		fail "Platform '$(uname)' not supported!" # TODO: call get_platform() instead
 		;;
 	esac
 
